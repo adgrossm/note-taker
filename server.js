42187128831e6139240cf3f42090a6f5cbf59032
Adam Grossman <adgrossm@gmail.com>
2020-09-02 17:07:56 -0400

    rebuilt folder structure

--- server.js
+++ server.js
@@ -2,37 +2,48 @@
 // =============================================================
 var express = require("express");
 var path = require("path");
-const fs = require("fs");
+var fs = require("fs");
 var db = require("./db/db.json")
+// not sure why I created the var below for index
+
+// var index = require("./assets/js/index")
+// do we need to require var index = require("./index")
 
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = 4000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(express.static("public"));
 // Routes
 // =============================================================
 function saveNotes() {
-    fs.writeFile(db, JSON.stringify(characters, null, 2), (err) => {
+    fs.writeFile("./db/db.json", JSON.stringify(characters, null, 2), (err) => {
         if (err) throw err;
     });
     }
+
 // Basic route that sends the user first to the AJAX Page
 // 
-app.get("/", function(req, res) {
-    // res.send("Welcome to the Star Wars Page!")
-    res.sendFile(path.join(__dirname, "index.html"));
+app.get("*", function(req, res) {
+    // should send to home page or index.html
+    res.sendFile(path.join(__dirname, "/index.html"));
   });
 
 
+app.get('/notes', function(req, res) {
+//  should send to notes page
+    res.sendFile(path.join(__dirname, "/notes.html"));
+  });
 // do we swap the api/characters for ./index or notes.html
-app.post("/api/characters", function(req, res) {
+app.post('/notes', function(req, res) {
     // req.body hosts is equal to the JSON post sent from the user
     // This works because of our body parsing middleware
+
+    // can we push the var from index.js of 
     var newcharacter = req.body;
     // console.log(newcharacter);
 //   how to create a new character ID so it creates the ID once it gets pushed to db.json array
@@ -41,7 +52,7 @@ app.post("/api/characters", function(req, res) {
     characters.push(newcharacter);
   
     //   important
-
+    console.log(newcharacter)
     saveNotes();
     // We then display the JSON to the users
     res.json(newcharacter);
@@ -52,7 +63,7 @@ app.post("/api/characters", function(req, res) {
       const id = req.params.id;
 
       for (let i = 0; i < characters.length; i++){
-       if (character[i] === id) {
+       if (characters[i] === id) {
            characters.splice(i);
        }  
        saveNotes();
@@ -65,11 +76,11 @@ app.post("/api/characters", function(req, res) {
 var characters ;
 // could we swap "db.json" just db for the var we declared with the path to bd.json
 
-fs.readFile("db.json", "utf8", (err, data)=> {
+fs.readFile("./db/db.json", "utf8", (err, data)=> {
     
         if (err) throw err;
-// **************   need to figure out what I need after characters - 
-characters -  JSON.parse(data);
+
+characters =  JSON.parse(data);
 });
 
 
